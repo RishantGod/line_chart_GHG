dadfd5e472c42c19a5db481dbbf1ec9a3bd08412
RishantGod <godararishant@gmail.com>
2025-01-24 11:08:23 +0530

    Temperature Line Graph Finished

---
+++ chart.js
@@ -0,0 +1,90 @@
+async function lineChart(){
+
+    //1. Fetch data
+    const yearFormat = d3.timeParse('%Y') 
+
+
+    const tempData = await d3.csv('temp.csv')
+
+    const xAccessor = d => yearFormat(d.Year)
+    const yAccessor1 = d => +d.temp
+
+    //2. Create chart dimensions
+
+    //1. DEFINING DIMENSIONS (FOR OUTER & INNER BOX)
+
+let dimensions = {
+    width: 600,
+    height: 400,
+    margin: {
+        top: 15,
+        right: 15,
+        bottom: 40,
+        left: 60,
+    },
+}
+
+dimensions.innerWidth = dimensions.width
+- dimensions.margin.left
+- dimensions.margin.right
+
+dimensions.innerHeight = dimensions.height
+- dimensions.margin.top
+- dimensions.margin.bottom
+
+// 2. OUTER BOX
+const outer = d3.select("#chart")
+        .append("svg")
+                .attr("width", dimensions.width)
+                .attr("height", dimensions.height)
+                
+// 3. INNER BOX
+
+const inner = outer.append("g")
+    .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+
+//3. Create scales
+
+const xScale = d3.scaleTime()
+    .domain(d3.extent(tempData, d => xAccessor(d)))
+    .range([0, dimensions.innerWidth])
+
+const tempScale = d3.scaleLinear()
+    .domain(d3.extent(tempData, d => yAccessor1(d)))
+    .range([dimensions.innerHeight, 0])
+
+
+//4. Draw data
+
+const tempLineGenerator = d3.line()
+    .x(d => xScale(xAccessor(d)))
+    .y(d => tempScale(yAccessor1(d)))
+
+
+const tempLine = inner.append('path')
+    .attr('d', tempLineGenerator(tempData))
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 2)
+
+//5. Draw peripherals
+
+const xAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.timeFormat('%Y'))
+
+const xAxisGroup = inner.append('g')
+    .call(xAxis)
+    .style('transform', `translateY(${dimensions.innerHeight}px)`)
+
+
+const yAxis = d3.axisLeft(tempScale)
+
+const yAxisGroup = inner.append('g')
+    .call(yAxis)
+
+
+
+
+} 
+
+lineChart();
