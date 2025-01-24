caf2611d1d4deb5c365610fa4666e461dbd61a9e
RishantGod <godararishant@gmail.com>
2025-01-24 12:14:52 +0530

    Added second Line

--- chart.js
+++ chart.js
@@ -5,16 +5,19 @@ async function lineChart(){
 
 
     const tempData = await d3.csv('temp.csv')
+    const coData = await d3.csv('co2.csv')
 
     const xAccessor = d => yearFormat(d.Year)
     const yAccessor1 = d => +d.temp
+
+    const yAccessor2 = d => +d.co2
 
     //2. Create chart dimensions
 
     //1. DEFINING DIMENSIONS (FOR OUTER & INNER BOX)
 
 let dimensions = {
-    width: 600,
+    width: 700,
     height: 400,
     margin: {
         top: 15,
@@ -53,12 +56,27 @@ const tempScale = d3.scaleLinear()
     .domain(d3.extent(tempData, d => yAccessor1(d)))
     .range([dimensions.innerHeight, 0])
 
+const coScale = d3.scaleLinear()
+    .domain(d3.extent(coData, d => yAccessor2(d)))
+    .range([dimensions.innerHeight, 0])
+
 
 //4. Draw data
 
 const tempLineGenerator = d3.line()
     .x(d => xScale(xAccessor(d)))
     .y(d => tempScale(yAccessor1(d)))
+
+const coLineGenerator = d3.line()
+    .x(d => xScale(xAccessor(d)))
+    .y(d => coScale(yAccessor2(d)))
+
+
+const coLine = inner.append('path')
+    .attr('d', coLineGenerator(coData))
+    .attr('fill', 'none')
+    .attr('stroke', 'blue')
+    .attr('stroke-width', 2)
 
 
 const tempLine = inner.append('path')
@@ -76,11 +94,18 @@ const xAxisGroup = inner.append('g')
     .call(xAxis)
     .style('transform', `translateY(${dimensions.innerHeight}px)`)
 
+const xAxisLabel = xAxisGroup.append('text')
+    .attr('x', dimensions.innerWidth / 2)
+    .attr('y', dimensions.margin.bottom - 10)
+    .html('Years')
+
 
 const yAxis = d3.axisLeft(tempScale)
 
 const yAxisGroup = inner.append('g')
     .call(yAxis)
+
+
 
 
 
